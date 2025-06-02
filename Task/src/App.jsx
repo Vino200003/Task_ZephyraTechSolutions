844c80a886f02dafdffc8733e0925e48a8abc3fd
Vino200003 <vinoshii03@gmail.com>
2025-06-02 17:00:55 +0530

    Login Form main structure

--- Task/src/App.jsx
+++ Task/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div className="app-container">
       <Header />
-      
+      <LoginForm/>
     </div>
   );
 }
---
+++ Task/src/components/LoginForm.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import '../styles/LoginForm.css'
+
+const LoginForm = () => {
+    return(
+        <div className="loginForm">
+            <div className="formContent">
+                <p className="startForFree">Start For Free</p>
+                <h1 className="createAcc">Create New Account</h1>
+                <p className="loginLink">Already have a Member?<a href="login">Log in</a></p>
+                <div className="mainForm">
+                    <div className="name">
+                        <input type="text" placeholder="First name" ></input>
+                        <input type="text" placeholder="Last Name"></input>
+                    </div>
+                    <div className="mailPw">
+                        <input type="email" placeholder="Enter your mail address"></input>
+                        <input type="password" placeholder="Enter your Password"></input>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginForm;
